07b0d33f5c4736dea782253095033d2bb7c676a4
Johan Donné <johan.donne@odisee.be>
2022-08-09 23:07:44 +0200

    Refactored Mp3FileWriter to use memorystream instead of Managed Queue.

--- AudioTools/Implementation/Mp3FileWriter.cs
+++ AudioTools/Implementation/Mp3FileWriter.cs
@@ -2,43 +2,62 @@
 using NAudio.MediaFoundation;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AudioTools.Implementation;
-public class Mp3FileWriter : IMp3FileWriter, IWaveProvider, ISampleProvider
+public class Mp3FileWriter : IMp3FileWriter, IWaveProvider, ISampleProvider, IDisposable
 {
-    private Queue<AudioSampleFrame>?_samples = new();
+    private MemoryStream? _memory = new();
+    private BinaryWriter? _writer;
+    private BinaryReader? _reader;
+    private bool _closed = false;
+    private bool disposedValue;
     private readonly SampleToWaveProvider _converter;
-    
+
     public int Samplerate => 44100;
 
     public string FilePath { get; }
 
-    public WaveFormat WaveFormat => WaveFormat.CreateIeeeFloatWaveFormat(44100,2);
+    public WaveFormat WaveFormat => WaveFormat.CreateIeeeFloatWaveFormat(44100, 2);
 
     public Mp3FileWriter(string filePath)
     {
         FilePath = filePath;
         _converter = new(this as ISampleProvider);
+        _memory = new MemoryStream();
+        _writer = new BinaryWriter(_memory);
         MediaFoundationApi.Startup();
+
     }
 
     public void Close()
     {
-        using var output = new FileStream(FilePath, FileMode.Create);
-        MediaFoundationEncoder.EncodeToMp3(this, output);
-        _samples = null;
+        if (_closed)
+        {
+            return;
+        }
+
+        _closed = true;
+        _writer?.Flush();
+        _memory?.Seek(0, SeekOrigin.Begin);
+        using (_reader = new(_memory!))
+        {
+            using FileStream? output = new(FilePath, FileMode.Create);
+            MediaFoundationEncoder.EncodeToMp3(this, output);
+        }
+        _reader = null;
+        _writer?.Dispose();
+        _writer = null;
+        _memory?.Dispose();
+        _memory = null;
     }
 
-    
     public void WriteSampleFrame(AudioSampleFrame frame)
     {
-        if (_samples == null) _samples = new();
-        _samples.Enqueue(frame);
+        if (!_closed)
+        {
+            _writer!.Write(frame.Left);
+            _writer!.Write(frame.Right);
+        }
     }
 
     //Implement IWaveProvider
@@ -47,17 +66,36 @@ public class Mp3FileWriter : IMp3FileWriter, IWaveProvider, ISampleProvider
         return _converter.Read(buffer, offset, count);
     }
 
-    
     // Implement ISampleProvider
     public int Read(float[] buffer, int offset, int count)
     {
-        int samplesRead = 0;
-        while ((samplesRead < count-2) && (_samples!.Count>0))
+        int index = 0;
+        while ((_reader!.BaseStream.Position != _reader.BaseStream.Length) && (index < count))
         {
-            var sample = _samples.Dequeue();
-            buffer[samplesRead++] = sample.Left;
-            buffer[samplesRead++] = sample.Right;
+            buffer[index++] = _reader.ReadSingle();
+            buffer[index++] = _reader.ReadSingle();
         }
-        return samplesRead;
-    }    
+        return index;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            _reader?.Dispose();
+            _writer?.Dispose();
+            _memory?.Dispose();
+            _reader = null;
+            _writer = null;
+            _memory = null;
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
 }
